8ad5cd03c93ed6e7ded7a70630751b4302b610d8
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2018-12-16 09:08:36 -0500

    Basic printing on editor changes

---
+++ spec/code-signature-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeSignatureView from '../lib/code-signature-view';
+
+describe('CodeSignatureView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
